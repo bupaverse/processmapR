8022ea2f33ed9fac05db46a37955727189b04d27
JANSSENSWILLEN Gert <gert.janssenswillen@uhasselt.be>
2020-04-30 17:02:40 +0200

    update lined chart

--- R/lined_chart.R
+++ R/lined_chart.R
@@ -8,49 +8,45 @@
 #'
 
 
-lined_chart <- function(eventlog, color, plotly,  ...) {
+lined_chart <- function(eventlog,
+						x = c("absolute","relative"),
+						sort = NULL,
+						color,
+						units,
+						line_width = 2,
+						plotly,
+						...) {
 	UseMethod("lined_chart")
 }
 
 
 # compute data for dotted_chart
 #' @export
-lined_chart.eventlog <- function(eventlog, color = NULL, plotly, ...) {
+lined_chart.eventlog <- function(eventlog,
+								 x = c("absolute","relative"),
+								 sort = NULL,
+								 color = NULL,
+								 units = c("weeks","days","hours","mins","secs"),
+								 line_width = 2,
+					  		 	plotly = FALSE, ...) {
 
-	if(is.null(color)) {
-		eventlog %>%
-			group_by_case() -> eventlog
+	x <- match.arg(x)
+	units <- match.arg(units)
+	mapping <- mapping(eventlog)
+
+	if(is.null(sort)) {
+		y <-	switch(x,
+					"absolute" = "start",
+					"relative" =  "duration")
 	} else {
-		eventlog %>% group_by( !!case_id_(eventlog), !!sym(color)) %>% summarize() %>% summarize(n = n()) %>% filter(n > 1) -> filter_color
-
-		if(nrow(filter_color) > 0) {
-			stop("Attribute given to color argument is not a case attribute")
-		} else {
-			eventlog %>% group_by(!!sym(color),
-								  !!case_id_(eventlog)) -> eventlog
-		}
+		y <-	match.arg(sort, choices = c("start","duration", "end"))
 	}
-
 
 
 	eventlog %>%
-		summarize(min = min(!!timestamp_(eventlog)), max = max(!!timestamp_(eventlog))) %>%
-		ggplot(aes(x = min, xend = max, y = fct_reorder(!!case_id_(eventlog), desc(min)), yend = fct_reorder(!!case_id_(eventlog), desc(min)),
-				   group = !!case_id_(eventlog))) +
-		scale_y_discrete(breaks = NULL) +
-		labs(x = "Time",y = "Cases") +
-		theme_light() -> p
+		lined_chart_data(color, units) %>%
+		lined_chart_plot(mapping, x, y, col_vector(), ifelse(is.null(color), activity_id(eventlog), color), units, line_width = line_width)
 
-
-	if(!is.null(color)) {
-		p <- p +
-			geom_segment(aes(color = factor(!!sym(color))))
-	} else {
-		p <- p +
-			geom_segment()
-	}
-
-	return(p)
 
 }
 
@@ -58,6 +54,91 @@ col_vector <- function() {
 	qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 	unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 }
+
+lined_chart_data <- function(eventlog, color, units) {
+	start_case_rank <- NULL
+	start <- NULL
+	end <- NULL
+	start_case <- NULL
+	end_case <- NULL
+
+	if(is.null(color)) {
+		eventlog %>%
+			mutate(color = !!activity_id_(eventlog)) -> eventlog
+	} else if(is.na(color)) {
+		eventlog %>%
+			mutate(color = "undefined") -> eventlog
+	} else {
+		eventlog %>%
+			mutate(color = !!sym(color)) -> eventlog
+	}
+
+	eventlog %>%
+		as.data.frame() %>%
+		group_by(!!case_id_(eventlog),!!activity_id_(eventlog),!!activity_instance_id_(eventlog), color, add = T) %>%
+		summarize(start = min(!!timestamp_(eventlog)),
+				  end = max(!!timestamp_(eventlog))) %>%
+		group_by(!!case_id_(eventlog)) -> grouped_activity_log
+
+
+	grouped_activity_log %>%
+		arrange(start) %>%
+		mutate(rank = paste0("ACTIVITY_RANKED_AS_", 1:n())) %>%
+		ungroup() %>%
+		select(!!case_id_(eventlog), rank, start) %>%
+		spread(rank, start) %>%
+		arrange_if(str_detect(names(.), "ACTIVITY_RANKED_AS_")) %>%
+		mutate(start_case_rank = 1:n()) %>%
+		select(!!case_id_(eventlog), start_case_rank) -> eventlog_rank_start_cases
+
+	grouped_activity_log %>%
+		mutate(start_case = min(start),
+			   end_case = max(end),
+			   dur = as.double(end_case - start_case, units = units)) %>%
+		mutate(start_relative = as.double(start - start_case, units = units),
+			   end_relative = as.double(end - start_case, units = units)) %>%
+		full_join(eventlog_rank_start_cases)
+}
+
+configure_x_aes_lined <- function(x) {
+	case_when(x == "absolute" ~ c("start","end"),
+			  x == "relative" ~ c("start_relative", "end_relative"))
+}
+
+configure_y_aes_lined <- function(y) {
+	case_when(y == "start" ~ "start_case_rank",
+			  y == "end" ~ "end_case",
+			  y == "duration" ~ "dur")
+}
+
+configure_x_labs_lined <- function(x, units) {
+	case_when(x == "relative" ~ as.character(glue("Time since start case (in {units})")),
+			  x == "absolute" ~ "Time")
+}
+
+
+lined_chart_plot <- function(data, mapping, x, y, col_vector, col_label, units, line_width) {
+
+	color <- NULL
+	x_aes <- configure_x_aes_lined(x)
+	y_aes <- configure_y_aes_lined(y)
+	x_labs <- configure_x_labs_lined(x, units)
+
+	data %>%
+		ggplot(aes_string(x = x_aes[[1]],
+						  xend = x_aes[[2]],
+						  y = glue("reorder({case_id(mapping)}, desc({y_aes}))"),
+						  yend = glue("reorder({case_id(mapping)}, desc({y_aes}))"))) +
+		scale_y_discrete(breaks = NULL) +
+		labs(x = x_labs,y = "Cases") +
+		theme_light() -> p
+
+	p + geom_segment(aes(color = color), lwd = line_width) +
+		scale_color_manual(name = col_label, values = col_vector)
+
+
+}
+
 
 #' @export
 
